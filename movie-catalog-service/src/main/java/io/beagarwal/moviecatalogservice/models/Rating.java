9d421064320e43cafee44299aa958815aecc5865
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-04-18 02:56:49 +0530

    Rating model added

---
+++ movie-catalog-service/src/main/java/io/beagarwal/moviecatalogservice/models/Rating.java
@@ -0,0 +1,30 @@
+package io.beagarwal.moviecatalogservice.models;
+
+public class Rating {
+    private String movieID;
+    private int rating;
+
+    public Rating() {
+
+    }
+    public Rating(String movieID, int rating) {
+        this.movieID = movieID;
+        this.rating = rating;
+    }
+
+    public String getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(String movieID) {
+        this.movieID = movieID;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
