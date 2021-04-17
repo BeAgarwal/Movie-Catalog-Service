62b2439070294c567cd3c94ff368f26261f935f0
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-04-18 03:01:38 +0530

    UserRating model added

---
+++ rating-data-service/src/main/java/io/beagarwal/ratingdataservice/models/UserRating.java
@@ -0,0 +1,16 @@
+package io.beagarwal.ratingdataservice.models;
+
+import java.util.List;
+
+public class UserRating {
+
+    private List<Rating> userRating;
+
+    public List<Rating> getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(List<Rating> userRating) {
+        this.userRating = userRating;
+    }
+}
