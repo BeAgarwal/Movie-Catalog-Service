0948e2834a351d82d63e237d9f389838ba781587
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-04-19 03:37:13 +0530

    User Rating model updated

--- movie-catalog-service/src/main/java/io/beagarwal/moviecatalogservice/models/UserRating.java
+++ movie-catalog-service/src/main/java/io/beagarwal/moviecatalogservice/models/UserRating.java
@@ -1,19 +1,35 @@
 package io.beagarwal.moviecatalogservice.models;
 
+import java.util.Arrays;
 import java.util.List;
+
 
 public class UserRating {
 
-    public UserRating() {
+    private String userID;
+    private List<Rating> ratings;
 
-    }
-    private List<Rating> userRating;
-
-    public List<Rating> getUserRating() {
-        return userRating;
+    public String getUserId() {
+        return userID;
     }
 
-    public void setUserRating(List<Rating> userRating) {
-        this.userRating = userRating;
+    public void setUserId(String userID) {
+        this.userID = userID;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    public void initData(String userId) {
+        this.setUserId(userId);
+        this.setRatings(Arrays.asList(
+                new Rating("100", 3),
+                new Rating("200", 4)
+        ));
     }
 }
--- rating-data-service/src/main/java/io/beagarwal/ratingdataservice/models/UserRating.java
+++ rating-data-service/src/main/java/io/beagarwal/ratingdataservice/models/UserRating.java
@@ -1,16 +1,34 @@
 package io.beagarwal.ratingdataservice.models;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class UserRating {
 
-    private List<Rating> userRating;
+    private String userID;
+    private List<Rating> ratings;
 
-    public List<Rating> getUserRating() {
-        return userRating;
+    public String getUserId() {
+        return userID;
     }
 
-    public void setUserRating(List<Rating> userRating) {
-        this.userRating = userRating;
+    public void setUserId(String userID) {
+        this.userID = userID;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    public void initData(String userId) {
+        this.setUserId(userId);
+        this.setRatings(Arrays.asList(
+                new Rating("100", 3),
+                new Rating("200", 4)
+        ));
     }
 }
