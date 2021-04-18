a6d4ac9cf00a271248b615a4e4cd339588eb6f67
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-04-19 02:17:28 +0530

    changed hardcore url to eureka discovery server (instance)

--- movie-catalog-service/src/main/java/io/beagarwal/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/beagarwal/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,8 +1,8 @@
 package io.beagarwal.moviecatalogservice.resources;
 
+import com.netflix.discovery.DiscoveryClient;
 import io.beagarwal.moviecatalogservice.models.CatalogItem;
 import io.beagarwal.moviecatalogservice.models.Movie;
-import io.beagarwal.moviecatalogservice.models.Rating;
 import io.beagarwal.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,15 +22,18 @@ public class MovieCatalogResource {
     private RestTemplate restTemplate;
 
     @Autowired
-    private WebClient.Builder webClientbuider;
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private WebClient.Builder webClientbuilder;
 
     @RequestMapping("/{userID}")
     public List<CatalogItem> getCatalog(@PathVariable("userID") String userID) {
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingdata/users/" + userID, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratingdata/users/" + userID, UserRating.class);
         return ratings.getUserRating().stream().map(rating -> {
             //for each movie ID, call MovieInfo service and get details
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieID(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieID(), Movie.class);
 
             //put them all together
             return new CatalogItem(movie.getName(), "Description", rating.getRating());
@@ -39,13 +41,3 @@ public class MovieCatalogResource {
 
     }
 }
-
-/*
-            Movie movie = webClientbuider.build()
-                    .get()
-                    .uri("http://localhost:8082/movies/" + rating.getMovieID())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
-
-             */
